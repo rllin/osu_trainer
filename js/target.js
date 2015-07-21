bc79d2fe0a1024d1fb80159192d904a2563f4799
rllin <aznpwnzor@gmail.com>
2015-07-21 14:03:14 -0700

    missed osme files

--- js/target.js
+++ js/target.js
@@ -1,11 +1,99 @@
+var circle;
+var inputFlag = 1;
+var mouseCoor;
+var viewportwidth;
+var viewportheight;
+updateSize();
+$(window).on('resize', function() {
+    updateSize();
+});
 
 var sections = [
     {'name': 'round 1',
-     'circles': ,
-    }
+     'circles': [[0.1, 0.1], [0.1, 0.2], [0.3, 0.4]],
+     'radius': 30,
+     'isi': 1000,
+     'rest': 3,
+    },
+    {'name': 'round 2',
+     'circles': [[0.1, 0.1], [0.1, 0.2], [0.3, 0.4]],
+     'radius': 30,
+     'isi': 1000,
+     'rest': 2,
+    },
+    {'name': 'download'}
 ];
 
+function submitMobaForm() {
+    $('#btnSubmitMoba').css('display', 'none')
+    $('#btnSubmit').css('display', 'block')
+    $('#bioForm').css('display', 'block')
+    $('#mobaForm').css('display', 'none')
+    if ($('#moba').val() == 'League') {
+        $('#mmrDesc').text('League MMR (use op.gg to estimate):')
+    } else if ($('#moba').val() == 'Dota2') {
+        $('#mmrDesc').text('Dota 2 MMR (exact value from profile):')
+    } else if ($('#moba').val() == 'CS:GO') {
+        $('#mmrDesc').text('CSGO rank:')
+    }
+}
 
+function submitForm() {
+    var email = $('#email').val();
+    var age = $('#age').val();
+    var gender = $('#gender').val();
+    var rank = $('#mmr').val();
+    var game = $('#moba').val();
+    var data = {
+        'game': game,
+        'email': email,
+        'age': age,
+        'gender': gender,
+        'rank': rank,
+    }
+    bio = data;
+    $.post('http://localhost:3000/api/target', {
+        'email': bio.email, 
+        'age': bio.age,
+        'gender': bio.gender,
+        'rank': bio.rank,
+        'data': JSON.stringify(data)
+    }).done(function(data) {
+        $('#required').css('display', 'none');
+        $('#btnSubmit').css('display', 'none');
+        $('#bioForm').css('display', 'none');
+        $('#headertext').css('display', 'none');
+        nextButton(sections[taskNum]);
+    }).fail(function(data) {
+        $('#required').text('Make sure you fill out all form boxes.');
+        $('#required').css('display', 'block');
+    });
+}
+
+var responseData = {};
+
+function addToResponseData(timestamp, state, data) {
+    $.post('http://localhost:3000/api/target', {
+        'email': bio.email, 
+        'age': bio.age,
+        'gender': bio.gender,
+        'rank': bio.rank,
+        'state': state,
+        'data': JSON.stringify(data)
+    }).done(function(data) {
+        console.log('inserted for ' + bio.email);
+    });
+}
+
+function nextButton(section) {
+    $('#btnSections').off();
+    $('#btnSections').on('click', function() {initialize(section)});
+    $('#btnSections').css('display', 'block');
+    $('#btnSections').text(section.name);
+}
+
+
+var taskNum = 0;
 
 var circlesClicked = 0;
 var totalCircles = 0;
@@ -23,17 +111,19 @@ var trainingEnded = 0;
 var responseData = {};
 
 function disableSelection(target) {
-    if (typeof target.onselectstart!="undefined") // if IE
-        target.onselectstart=function(){return false}
-    else if (typeof target.style.MozUserSelect!="undefined") // if Firefox
-        target.style.MozUserSelect="none";
-    else // others
-        target.onmousedown=function(){return false;}
+    target.attr('unselectable', 'on')
+        .css({'-moz-user-select':'-moz-none',
+           '-moz-user-select':'none',
+           '-o-user-select':'none',
+           '-khtml-user-select':'none',
+           '-webkit-user-select':'none',
+           '-ms-user-select':'none',
+           'user-select':'none'
+     }).bind('selectstart', function(){ return false; });
 }
 
 function calculate() {
-	var d = $("#playback")[0];
-	var playbackRate = d.options[d.selectedIndex].value;
+	var playbackRate = $('#playback').val();
 	var bpm_new = 100*playbackRate;
 	var beats_per_second = bpm_new/60;
 	milliseconds_per_beat = 1000/beats_per_second;
@@ -50,9 +140,9 @@ function resetTable() {
 
 function startCountdown() {
 	calculate();
-	disableSelection($("#purple")[0]);
-	disableSelection($("#timer")[0]);
-	disableSelection($("#circlestats")[0]);
+	disableSelection($("#purple"));
+	disableSelection($("#timer"));
+	disableSelection($("#circlestats"));
 	$("#tableContent").css('visibility', "hidden");
 	$("#countdown").innerHTML = "Ready?";
 	countdownTimer = setInterval(function() { handleTimer(countdownCount); }, 700);
@@ -70,21 +160,133 @@ function handleTimer() {
 
 function endCountdown() {
 	$("#content").css('visibility', "hidden");
-	//var d = $("#playback");
-	//$("#player").playbackRate = d.options[d.selectedIndex].value;
-	var e = $("#duration");
-	var f = $("#size");
-	startLoop(f.options[f.selectedIndex].value,e.options[e.selectedIndex].value);
+	startLoop($('#size').val(), $('#duration').val());
 }
 
-function startLoop(size,duration) {
-	$("#timer").style.visibility = "hidden";
-	$("#circlestats").style.visibility = "visible";
-	$("#accuracy").style.visibility = "hidden";
-	$("#averageAccuracy").style.visibility = "hidden";
-	$("#purple").style.visibility = "visible";
-	$("#purple").style.height = 100*size+"px";
-	$("#purple").style.width = 100*size+"px";
+function updateSize() {
+    if (typeof window.innerWidth != 'undefined') {
+        viewportwidth = window.innerWidth,
+        viewportheight = window.innerHeight
+    }
+    else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
+        viewportwidth = document.documentElement.clientWidth,
+        viewportheight = document.documentElement.clientHeight
+    }
+    else {
+        viewportwidth = document.getElementsByTagName('body')[0].clientWidth,
+        viewportheight = document.getElementsByTagName('body')[0].clientHeight
+    }
+}
+
+function initialize(section) {
+    var mode = section.name;
+    var circles = section.circles;
+    var size = 2*section.radius;
+    console.log('initialize')
+ 
+	$("#purple").css('height', size+"px");
+	$("#purple").css('width', size+"px");
+    $('#btnSections').css('display', 'none');
+    taskNum++;
+    var data = {
+        'task mode': mode,
+        'combos': circles,
+        'size': {viewportheight, viewportwidth}
+    };
+    addToResponseData((new Date()).getTime().toString(), 'initialize', data);
+    circleList = circles.slice(0);
+    //circle = circleList.shift();
+    showNextCircle(section);
+}
+
+function showNextCircle(section) {
+    mouseCoor = [];
+    $(document).off();
+    $(document).on('mousemove', function(event) {
+        var nCoor = normalizeCoors(event.pageX, event.pageY);
+        var nxCoor = nCoor.x,
+            nyCoor = nCoor.y;
+        mouseCoor.push([(new Date).getTime(), 'move', nxCoor, nyCoor])
+    });
+    $(document).on('click', function(event) {
+        var nCoor = normalizeCoors(event.pageX, event.pageY);
+        var nxCoor = nCoor.x,
+            nyCoor = nCoor.y;
+        mouseCoor.push([(new Date).getTime(), 'click', nxCoor, nyCoor])
+    });
+    circle = circleList.shift();
+    setTimeout(function() {
+        if (circle == undefined && circleList.length == 0) {
+            endGame(taskNum, section);
+        } else {
+            inputFlag = 1;
+            var cCoor = convertCoors(circle[0], circle[1]);
+            var txCoor = cCoor.x,
+                tyCoor = cCoor.y;
+            $(document).on('click', function(event) {execute(event, section, circle)});
+            $('#purple').css('background-color', '#000000');
+            $('#purple').css('display', 'block');
+            $("#purple").css('left', txCoor - section.radius + "px");
+            $("#purple").css('top', tyCoor - section.radius + "px");
+        }
+    }, section.isi);
+}
+
+function normalizeCoors(xCoor, yCoor) {
+    return {x: xCoor / viewportwidth, y: yCoor / viewportheight}
+} 
+
+function convertCoors(xCoor, yCoor) {
+    return {x: xCoor * viewportwidth, y: yCoor * viewportheight}
+}
+    
+function execute(event, section, circle) {
+    var timestamp = (new Date()).getTime().toString();
+    var xCoor = event.pageX
+    var yCoor = event.pageY
+    var cCoor = convertCoors(circle[0], circle[1]);
+    var txCoor = cCoor.x,
+        tyCoor = cCoor.y;
+    if ((xCoor - txCoor)*(xCoor - txCoor) + (yCoor - tyCoor)*(yCoor - tyCoor) <= section.radius*section.radius && inputFlag == 1) {
+        $('#purple').css('background-color', '#00FF00');
+        inputFlag = 0;
+        var data = {
+            'mouseCoor': mouseCoor,
+        }
+        console.log('last mouseCoor: ' + mouseCoor[mouseCoor.length - 1])
+        addToResponseData(timestamp, 'hit', data);
+        showNextCircle(section);
+    } else {
+        console.log('clicked out circle: ' + xCoor + ', ' +  yCoor)
+    }
+}
+
+function endGame(nextTask) {
+    $(document).off();
+    console.log(sections[taskNum].name);
+    $('#purple').css('display', 'none');
+    if (sections[taskNum].name == 'download') {
+        $('#btndownload').html('<a href="data:' + encodeURI("text/json;charset=utf-8," + JSON.stringify(responseData)) + '" download="' + bio['email'].replace('@', '_').replace('.', '_') + '.json">Download Json</a>');
+        $('#btndownload').css('display', 'block');
+        $('#doneInstructions').css('display', 'block');
+    } else {
+        var myCounter = new Countdown({  
+            seconds: sections[taskNum].rest,  // number of seconds to count down
+            onUpdateStatus: function(sec){$('#countDown').text(sec + ' seconds until next session.');}, // callback for each second
+            onCounterEnd: function() {
+                alert('Start next session!');
+                $('#countDown').text('');
+                nextButton(sections[taskNum]);
+            } // final action
+        });
+        myCounter.start();
+    }   
+}
+
+
+
+
+function startLoop(size, duration) {
 	trainingTimer(duration,0);
 	circleTimer(size,0);
 }
@@ -102,11 +304,11 @@ function trainingTimer(duration,c) {
 function endTraining() {
 	trainingEnded = 1;
     setTimeout(function() {
-        $("#timer").style.visibility = "hidden";
-        $("#circlestats").style.visibility = "hidden";
-        $("#accuracy").style.visibility = "hidden";
-        $("#averageAccuracy").style.visibility = "hidden";
-        $("#content").style.visibility = "visible";
+        $("#timer").css('display', 'none');
+        $("#circlestats").css('display', 'none');
+        $("#accuracy").css('display', 'none');
+        $("#averageAccuracy").css('display', 'none');
+        $("#content").css('display', 'block');
         $("#headertext").innerHTML = "TIME IS UP!";
         //$("#countdown").innerHTML = "Results:<br/><br/>"+$("#circlestats").innerHTML+"<br/>"+$("#averageAccuracy").innerHTML+"<br/><br/><span onclick='location.reload()' style='cursor:pointer'>Play Again</span>";
         $("#countdown").innerHTML = "<span onclick='location.reload()' style='cursor:pointer'>Play Again</span>";
@@ -169,7 +371,7 @@ function circleTimer(size,c) {
 		totalCirclesDisplay = totalCircles;
 	}
 	$("#circlestats").innerHTML = "Circles clicked: "+circlesClicked+"/"+totalCirclesDisplay+" ("+Math.round((circlesClicked/totalCirclesDisplay*100))+"%)";
-    //document.getElementById('player').play();
+    //$('#player').play();
 	var bound = 100*size;
 	var viewportwidth;
 	var viewportheight;
@@ -188,21 +390,21 @@ function circleTimer(size,c) {
 	var rightBound = viewportwidth-bound;
 	var bottomBound = viewportheight-bound;
 
-    $("#purple").style.visibility = "visible";
+    $("#purple").css('display', 'block');
 
     purpleCoor = generateCircle(purpleX, purpleY, bound, rightBound, bottomBound)
     purpleX = purpleCoor.x;
     purpleY = purpleCoor.y;
 
-    $("#purple").style.left = purpleX + "px";
-    $("#purple").style.top = purpleY + "px";
+    $("#purple").css('left', purpleX + "px");
+    $("#purple").css('top', purpleY + "px");
     purpleTimer = new Date().getTime();
 
     responseData[purpleTimer] = {'click': 0}
 
 	if(trainingEnded==1) {
 		clearInterval(loopTimer);
-		$("#purple").style.visibility = "hidden";
+		$("#purple").css('display', 'none');
 	}
 	else {
 		loopTimer = setTimeout(function() {
@@ -219,7 +421,7 @@ function clickedCircle() {
     var averageTiming;
     var timing;
     accuracy = (currentTime - purpleTimer);
-    $("#purple").style.visibility = "hidden";
+    $("#purple").css('visibility', "hidden");
     if(accuracy>0) {
         timing = "early";
         timing = 'late';
@@ -246,3 +448,33 @@ function clickedCircle() {
         'circleY': purpleY,
     }
 }
+
+
+function Countdown(options) {
+  var timer,
+  instance = this,
+  seconds = options.seconds || 10,
+  updateStatus = options.onUpdateStatus || function () {},
+  counterEnd = options.onCounterEnd || function () {};
+
+  function decrementCounter() {
+    updateStatus(seconds);
+    if (seconds === 0) {
+      counterEnd();
+      instance.stop();
+    }
+    seconds--;
+  }
+
+  this.start = function () {
+    clearInterval(timer);
+    timer = 0;
+    seconds = options.seconds;
+    timer = setInterval(decrementCounter, 1000);
+  };
+
+  this.stop = function () {
+    clearInterval(timer);
+  };
+}
+
