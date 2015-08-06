759d0bdccc14a699278277bf9a9e37f42f13eb3c
Randall Lin <aznpwnzor@gmail.com>
2015-08-06 00:50:29 +0000

    more structure for stop and go and instead it is now go no go stop

--- target/db.js
+++ target/db.js
@@ -1,8 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-conn_targ = mongoose.createConnection('mongodb://localhost/target');
-conn_stopgo = mongoose.createConnection('mongodb://localhost/stopgo');
+//db = mongoose.connect('mongodb://localhost/target_second');
+dbTarg = mongoose.createConnection('mongodb://127.0.0.1/target_second');
+dbStop = mongoose.createConnection('mongodb://127.0.0.1/stopgo');
+
+dbTarg.on("open", function(ref) {console.log("Connected to mongo server Target collection.");});
+dbTarg.on("error", function(err) {console.log("Could not connect to mongo server Target collection!");});
+dbStop.on("open", function(ref) {console.log("Connected to mongo server Stop collection.");});
+dbStop.on("error", function(err) {console.log("Could not connect to mongo server Stop collection!");});
 
 var Target = new Schema({
     timestamp: {type: String, required: true},
@@ -10,15 +16,16 @@ var Target = new Schema({
     state: {type: String, required: true},
     content: {type: String, required: true}
 });
-var TargetModel = mongoose.model('Target', Target);
+var TargetModel = dbTarg.model('Target', Target);
+//var TargetModel = mongoose.model('Target', Target);
 
 var Stop = new Schema({
     timestamp: {type: String, required: true},
     email: {type: String, required: true},
     state: {type: String, required: true},
-    content: {type: String, required: true}
+    content: {type: Object, required: true}
 });
-var StopModel = mongoose.model('Stop', Stop);
+var StopModel = dbStop.model('Stop', Stop);
 
 module.exports.TargetModel = TargetModel;
 module.exports.StopModel = StopModel;
